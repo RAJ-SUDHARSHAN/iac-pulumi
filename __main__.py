eccb59c1115812a88eb5ece0ab40956c244f52f7
Raj Sudharshan <sudharshann05@gmail.com>
2023-10-27 04:44:48 -0700

    create rds instance and add dependency to ec2

--- __main__.py
+++ __main__.py
@@ -62,6 +62,9 @@ public_route = aws.ec2.Route(
     gateway_id=igw.id,
 )
 
+public_subnets = []
+private_subnets = []
+
 for i in range(num_azs):
     public_subnet = aws.ec2.Subnet(
         f"public_subnet-{i}",
@@ -73,6 +76,7 @@ for i in range(num_azs):
             "Name": f"{TAG_BASE_NAME}-public_subnet-{i}",
         },
     )
+    public_subnets.append(public_subnet)
 
     aws.ec2.RouteTableAssociation(
         f"public_subnet-{i}-rt-association",
@@ -90,16 +94,19 @@ for i in range(num_azs):
             "Name": f"{TAG_BASE_NAME}-private_subnet-{i}",
         },
     )
+    private_subnets.append(private_subnet)
 
     aws.ec2.RouteTableAssociation(
         f"private_subnet-{i}-rt-association",
         subnet_id=private_subnet.id,
         route_table_id=private_rt.id,
     )
-    
-INGRESS_PORTS = list(map(int, get_env_variable("INGRESS_PORTS").split(',')))
-SG_CIDR_BLOCK= get_env_variable("SG_CIDR_BLOCK")
-EGRESS_PORT = int(get_env_variable("EGRESS_PORT"))
+
+SG_CIDR_BLOCK = get_env_variable("SG_CIDR_BLOCK")
+APP_INGRESS_PORTS = list(map(int, get_env_variable("APP_INGRESS_PORTS").split(",")))
+APP_EGRESS_PORT = int(get_env_variable("APP_EGRESS_PORT"))
+DB_INGRESS_PORT = int(get_env_variable("DB_INGRESS_PORT"))
+DB_EGRESS_PORT = int(get_env_variable("DB_EGRESS_PORT"))
 
 app_security_group = aws.ec2.SecurityGroup(
     "application_security_group",
@@ -112,14 +119,14 @@ app_security_group = aws.ec2.SecurityGroup(
             "to_port": port,
             "cidr_blocks": [SG_CIDR_BLOCK],
         }
-        for port in INGRESS_PORTS
+        for port in APP_INGRESS_PORTS
     ],
     egress=[
         {
             "protocol": "-1",
-            "from_port": EGRESS_PORT,
-            "to_port": EGRESS_PORT,
-            "cidr_blocks": [SG_CIDR_BLOCK]
+            "from_port": APP_EGRESS_PORT,
+            "to_port": APP_EGRESS_PORT,
+            "cidr_blocks": [SG_CIDR_BLOCK],
         }
     ],
     tags={
@@ -127,20 +134,121 @@ app_security_group = aws.ec2.SecurityGroup(
     },
 )
 
+db_security_group = aws.ec2.SecurityGroup(
+    "db_security_group",
+    vpc_id=my_vpc.id,
+    description="Security group for RDS database",
+    ingress=[
+        {
+            "protocol": "tcp",
+            "from_port": DB_INGRESS_PORT,
+            "to_port": DB_INGRESS_PORT,
+            "security_groups": [app_security_group.id],
+        }
+    ],
+    egress=[
+        {
+            "protocol": "-1",
+            "from_port": DB_EGRESS_PORT,
+            "to_port": DB_EGRESS_PORT,
+            "cidr_blocks": [SG_CIDR_BLOCK],
+        }
+    ],
+    tags={
+        "Name": f"{TAG_BASE_NAME}-db_security_group",
+    },
+)
+
+db_param_grp = aws.rds.ParameterGroup(
+    "rds-parameter-group",
+    family="postgres15",
+    description="Custom parameter group for csye6225",
+    tags={
+        "Name": f"{TAG_BASE_NAME}-parameter-group",
+    },
+)
+
+db_subnet_group = aws.rds.SubnetGroup(
+    "db_subnet_group",
+    subnet_ids=[subnet.id for subnet in private_subnets],
+    tags={
+        "Name": f"{TAG_BASE_NAME}-db-subnet-group",
+    },
+)
+
+RDS_DB_NAME = get_env_variable("RDS_DB_NAME")
+RDS_ENGINE = get_env_variable("RDS_ENGINE")
+RDS_ENGINE_VERSION = get_env_variable("RDS_ENGINE_VERSION")
+RDS_INSTANCE_CLASS = get_env_variable("RDS_INSTANCE_CLASS")
+RDS_STORGAE = get_env_variable("RDS_STORGAE")
+RDS_IDENTIFIER = get_env_variable("RDS_IDENTIFIER")
+RDS_USERNAME = get_env_variable("RDS_USERNAME")
+RDS_DB_PASSWORD = get_env_variable("RDS_DB_PASSWORD")
+RDS_MULTI_AZ = get_env_variable("RDS_MULTI_AZ")
+RDS_PUBLICLY_ACCESSIBLE = get_env_variable("RDS_PUBLICLY_ACCESSIBLE")
+RDS_PORT = get_env_variable("RDS_PORT")
+USERDATA_USER = get_env_variable("USERDATA_USER")
+USERDATA_GROUP = get_env_variable("USERDATA_GROUP")
+
+rds_instance = aws.rds.Instance(
+    "csye6225-rds-instance",
+    db_name=RDS_DB_NAME,
+    allocated_storage=RDS_STORGAE,
+    engine=RDS_ENGINE,
+    engine_version=RDS_ENGINE_VERSION,
+    instance_class=RDS_INSTANCE_CLASS,
+    identifier=RDS_IDENTIFIER,
+    parameter_group_name=db_param_grp.name,
+    username=RDS_USERNAME,
+    password=RDS_DB_PASSWORD,
+    skip_final_snapshot=True,
+    vpc_security_group_ids=[db_security_group.id],
+    db_subnet_group_name=db_subnet_group.name,
+    tags={
+        "Name": f"{TAG_BASE_NAME}-rds-instance",
+    },
+    multi_az=RDS_MULTI_AZ,
+    publicly_accessible=RDS_PUBLICLY_ACCESSIBLE,
+)
+
+
+def generate_user_data_script(rds_endpoint):
+    return f"""#!/bin/bash
+ENV_FILE="/opt/webapp.properties"
+echo "DB_USERNAME={RDS_USERNAME}" > $ENV_FILE
+echo "DB_PASSWORD={RDS_DB_PASSWORD}" >> $ENV_FILE
+echo "DB_HOSTNAME={rds_endpoint}" >> $ENV_FILE
+echo "FLASK_APP={get_env_variable('FLASK_APP')}" >> $ENV_FILE
+echo "FLASK_DEBUG={get_env_variable('FLASK_DEBUG')}" >> $ENV_FILE
+echo "DATABASE_URL=postgresql://{RDS_USERNAME}:{RDS_DB_PASSWORD}@{rds_endpoint}/{RDS_DB_NAME}" >> $ENV_FILE
+echo "CSV_PATH={get_env_variable('CSV_PATH')}" >> $ENV_FILE
+chown {USERDATA_USER}:{USERDATA_GROUP} $ENV_FILE
+sudo chown -R {USERDATA_USER}:{USERDATA_GROUP} /opt/webapp/
+sudo chown {USERDATA_USER}:{USERDATA_GROUP} /opt/users.csv
+chmod 400 $ENV_FILE
+sudo systemctl daemon-reload
+sudo systemctl start csye6225
+"""
+
+
+user_data_script = rds_instance.endpoint.apply(generate_user_data_script)
+
 app_instance = aws.ec2.Instance(
     "app_instance",
     ami=get_env_variable("AMI_ID"),
     instance_type=get_env_variable("INSTANCE_TYPE"),
     key_name=get_env_variable("KEY_NAME"),
     vpc_security_group_ids=[app_security_group.id],
-    subnet_id=public_subnet.id,
+    subnet_id=public_subnets[0].id,
     root_block_device={
         "volume_size": int(get_env_variable("ROOT_VOLUME_SIZE")),
         "volume_type": get_env_variable("ROOT_VOLUME_TYPE"),
-        "delete_on_termination": get_env_variable("DELETE_ON_TERMINATION"),  
+        "delete_on_termination": get_env_variable("DELETE_ON_TERMINATION"),
     },
     tags={
         "Name": f"{TAG_BASE_NAME}-app_instance",
     },
     disable_api_termination=get_env_variable("DISABLE_API_TERMINATION"),
+    opts=pulumi.ResourceOptions(depends_on=[rds_instance]),
+    user_data=user_data_script,
 )
